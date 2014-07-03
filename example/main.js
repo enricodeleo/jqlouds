00b9f8cd94a1f5d7630f5e6a0361d04672bb158e
Enrico Deleo <enrico@lysergicstudio.com>
2014-07-04 00:52:50 +0200

    it works, even if it isn't clean yet

---
+++ example/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+;(function($){
+
+$('#sky').jQlouds();
+
+})(jQuery);
--- src/jqlouds.js
+++ src/jqlouds.js
@@ -8,29 +8,103 @@
 
 ;(function ($) {
 
+  //we'll need random numbers
+  $.extend({
+    random: function(X) {
+      return Math.floor(X * (Math.random() % 1));
+    },
+    randomBetween: function(MinV, MaxV) {
+      return MinV + jQuery.random(MaxV - MinV + 1);
+    }
+  });
+
   // Collection method.
-  $.fn.awesome = function () {
+  $.fn.jQlouds = function (options) {
+    options = $.extend({}, $.jQlouds.options, options);
     return this.each(function (i) {
       // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
+      $.jQlouds.spawn(options);
     });
   };
 
   // Static method.
-  $.awesome = function (options) {
+  $.jQlouds = function (options) {
     // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
+    options = $.extend({}, $.jQlouds.options, options);
     // Return something awesome.
-    return 'awesome' + options.punctuation;
+    return function(){
+      console.log(options);
+    };
   };
 
   // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
+  $.jQlouds.options = {
+    cloudSrc: 'images/cloud.png',
+    cloudWidth: 227,
+    cloudHeight: 96,
+    randomClouds: $.randomBetween(20, 30),
+    skyHeight: $(this).height(),
+    skyWidth: $(this).width()
+  };
+
+  $.jQlouds.spawn = function(options) {
+    for (var i = 0; i < options.randomClouds; i++) {
+
+      var sizeRatio = $.randomBetween(1, 4),
+      cldWidth = Math.floor(options.cloudWidth/sizeRatio),
+      cldHeight = Math.floor(options.cloudHeight/sizeRatio),
+      cloud = new Image(cldWidth, cldHeight);
+      cloud.className += 'cloud-up';
+      cloud.src = options.cloudSrc;
+      cloud.style.position = 'absolute';
+      cloud.style.bottom = options.skyHeight/$.randomBetween(2, 4) + 'px';
+      if ( i % 2 === 0 ) {
+        cloud.style.left = (options.skyWidth/2) + (options.skyWidth/$.randomBetween(2, 20)) + 'px';
+      }
+      else {
+        cloud.style.left = options.skyWidth/$.randomBetween(2, 20) + 'px';
+      }
+
+      $('#sky').append(cloud);
+
+      //some css effect
+      $('.cloud-up').each(function(i, obj) {
+        var randBlur = $.randomBetween(1, 4),
+        randOpacity = '0.' + $.randomBetween(5, 8);
+        if( randBlur === 1 ) {
+          $(this).addClass('blur');
+        }
+        else if ( randBlur === 2 ) {
+          $(this).css({ opacity: randOpacity });
+        }
+      });
+
+      //ok first animation, then this event will be triggered every time the previous animation ends
+      $(document).trigger('animateNow');
+
+    }
+  };
+
+    //recall animation
+  $(document).on('animateNow', function() {
+
+    $('.cloud-up').each(function(i, obj) {
+      $.jQlouds.animateCloud($(this));
+    });
+
+  });
+
+  $.jQlouds.animateCloud = function(element) {
+    element.animate({left: '+='+$.randomBetween(10, 40)}, $.randomBetween(4000, 7000), 'linear').
+    animate({left: '+='+$.randomBetween(10, 40)}, $.randomBetween(4000, 7000), 'linear').
+    animate({left: '-='+$.randomBetween(10, 40)}, $.randomBetween(4000, 7000), 'linear').delay($.randomBetween(4000, 10000)).
+    animate({left: '-='+$.randomBetween(10, 40)}, $.randomBetween(4000, 7000), 'linear', function() {
+      $(document).trigger('animateNow');
+    });
   };
 
   // Custom selector.
-  $.expr[':'].awesome = function (elem) {
+  $.expr[':'].jQlouds = function (elem) {
     // Is this element awesome?
     return $(elem).text().indexOf('awesome') !== -1;
   };
