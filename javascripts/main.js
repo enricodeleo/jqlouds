af4878b9f1e56a50bff8b1f63cccb0494d5051ae
Enrico Deleo <enrico@lysergicstudio.com>
2014-07-06 02:45:50 +0200

    website is the same as 'example'

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,30 @@
-console.log('This would be the main JS file.');
+'use strict';
+
+;(function($){
+
+$('#sky').jQlouds({
+  src: 'images/cloud.png', // path to image
+  minClouds: 3, // minimum amount of clouds
+  maxClouds: 5, // maximum amount of clouds
+});
+
+$('#sky2').jQlouds({
+  maxWidth: 113, // max image's width
+  maxHeight: 48, // amx image's height
+  minClouds: 8, // minimum amount of clouds
+  maxClouds: 10, // maximum amount of clouds
+})
+
+$('#sky3').jQlouds({
+  minClouds: 8, // minimum amount of clouds
+  maxClouds: 10, // maximum amount of clouds
+  wind: true
+})
+
+})(jQuery);
+
+!function ($) {
+    $(function(){
+      window.prettyPrint && prettyPrint()
+    })
+}(window.jQuery);
